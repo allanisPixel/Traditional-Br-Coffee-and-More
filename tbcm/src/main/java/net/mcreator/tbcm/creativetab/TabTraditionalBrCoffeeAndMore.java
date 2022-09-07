13bc90797ae74a58f20039f0a45ccbdd3fc4897c
allanisPixel <48072649+allanisPixel@users.noreply.github.com>
2022-09-07 15:18:48 -0300

    Create creative tab and insert archives

---
+++ tbcm/src/main/java/net/mcreator/tbcm/creativetab/TabTraditionalBrCoffeeAndMore.java
@@ -0,0 +1,35 @@
+
+package net.mcreator.tbcm.creativetab;
+
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.relauncher.Side;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.init.Items;
+import net.minecraft.creativetab.CreativeTabs;
+
+import net.mcreator.tbcm.ElementsTbcmMod;
+
+@ElementsTbcmMod.ModElement.Tag
+public class TabTraditionalBrCoffeeAndMore extends ElementsTbcmMod.ModElement {
+	public TabTraditionalBrCoffeeAndMore(ElementsTbcmMod instance) {
+		super(instance, 1);
+	}
+
+	@Override
+	public void initElements() {
+		tab = new CreativeTabs("tabtraditional_br_coffee_and_more") {
+			@SideOnly(Side.CLIENT)
+			@Override
+			public ItemStack getTabIconItem() {
+				return new ItemStack(Items.EXPERIENCE_BOTTLE, (int) (1));
+			}
+
+			@SideOnly(Side.CLIENT)
+			public boolean hasSearchBar() {
+				return false;
+			}
+		};
+	}
+	public static CreativeTabs tab;
+}
