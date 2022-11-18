9854d2a95f93567689496b89ebb4d4180f7a120d
allanisPixel <48072649+allanisPixel@users.noreply.github.com>
2022-11-18 20:16:09 -0300

    A planta mais bugada dos 7 mares
    Existe um código parcial de criar uma planta (tô usando a batata por hora) mas está com 2 bugs (subtrair seed no criativo e quebrar o bloco de farm)

---
+++ tbcm/src/main/java/net/mcreator/tbcm/procedure/ProcedureCoffeeSeedsQuandoClicarComOBotaoDireitoNoBloco.java
@@ -0,0 +1,90 @@
+package net.mcreator.tbcm.procedure;
+
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.common.MinecraftForge;
+
+import net.minecraft.world.World;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.item.ItemStack;
+import net.minecraft.init.Blocks;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.Entity;
+
+import net.mcreator.tbcm.item.ItemCoffeeSeeds;
+import net.mcreator.tbcm.ElementsTbcmMod;
+
+import java.util.Map;
+
+@ElementsTbcmMod.ModElement.Tag
+public class ProcedureCoffeeSeedsQuandoClicarComOBotaoDireitoNoBloco extends ElementsTbcmMod.ModElement {
+	public ProcedureCoffeeSeedsQuandoClicarComOBotaoDireitoNoBloco(ElementsTbcmMod instance) {
+		super(instance, 11);
+	}
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			System.err.println("Failed to load dependency entity for procedure CoffeeSeedsQuandoClicarComOBotaoDireitoNoBloco!");
+			return;
+		}
+		if (dependencies.get("x") == null) {
+			System.err.println("Failed to load dependency x for procedure CoffeeSeedsQuandoClicarComOBotaoDireitoNoBloco!");
+			return;
+		}
+		if (dependencies.get("y") == null) {
+			System.err.println("Failed to load dependency y for procedure CoffeeSeedsQuandoClicarComOBotaoDireitoNoBloco!");
+			return;
+		}
+		if (dependencies.get("z") == null) {
+			System.err.println("Failed to load dependency z for procedure CoffeeSeedsQuandoClicarComOBotaoDireitoNoBloco!");
+			return;
+		}
+		if (dependencies.get("world") == null) {
+			System.err.println("Failed to load dependency world for procedure CoffeeSeedsQuandoClicarComOBotaoDireitoNoBloco!");
+			return;
+		}
+		Entity entity = (Entity) dependencies.get("entity");
+		int x = (int) dependencies.get("x");
+		int y = (int) dependencies.get("y");
+		int z = (int) dependencies.get("z");
+		World world = (World) dependencies.get("world");
+		if (((((entity instanceof EntityLivingBase) ? ((EntityLivingBase) entity).getHeldItemMainhand() : ItemStack.EMPTY)
+				.getItem() == new ItemStack(ItemCoffeeSeeds.block, (int) (1)).getItem())
+				&& (((world.getBlockState(new BlockPos((int) x, (int) y, (int) z))).getBlock() == Blocks.FARMLAND.getDefaultState().getBlock())
+						&& ((world.getBlockState(new BlockPos((int) x, (int) (y + 1), (int) z))).getBlock() == Blocks.AIR.getDefaultState()
+								.getBlock())))) {
+			world.setBlockToAir(new BlockPos((int) x, (int) (y + 1), (int) z));
+			world.setBlockState(new BlockPos((int) x, (int) y, (int) z), Blocks.POTATOES.getDefaultState(), 3);
+			if (true) {
+				if (entity instanceof EntityPlayer)
+					((EntityPlayer) entity).inventory.clearMatchingItems(
+							((entity instanceof EntityLivingBase) ? ((EntityLivingBase) entity).getHeldItemMainhand() : ItemStack.EMPTY).getItem(),
+							-1, (int) 1, null);
+			}
+		}
+	}
+
+	@SubscribeEvent
+	public void onRightClickItem(PlayerInteractEvent.RightClickItem event) {
+		EntityPlayer entity = event.getEntityPlayer();
+		int i = event.getPos().getX();
+		int j = event.getPos().getY();
+		int k = event.getPos().getZ();
+		World world = event.getWorld();
+		java.util.HashMap<String, Object> dependencies = new java.util.HashMap<>();
+		dependencies.put("x", i);
+		dependencies.put("y", j);
+		dependencies.put("z", k);
+		dependencies.put("world", world);
+		dependencies.put("entity", entity);
+		dependencies.put("event", event);
+		this.executeProcedure(dependencies);
+	}
+
+	@Override
+	public void preInit(FMLPreInitializationEvent event) {
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+}
